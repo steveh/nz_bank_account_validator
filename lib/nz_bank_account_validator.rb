e76b420b4203fd8dab84cd807d94e014d3d57b4e
Steve Hoeksema <steve@kotiri.com>
2016-08-31 10:52:52 +1200

    Add BankDefinition struct, Rubocop

--- lib/nz_bank_account_validator.rb
+++ lib/nz_bank_account_validator.rb
@@ -1,45 +1,49 @@
-require "nz_bank_account_validator/version"
-
 # frozen_string_literal: true
+require 'nz_bank_account_validator/version'
+
 class NzBankAccountValidator
+  BankDefinition = Struct.new(:ranges, :algo)
+
   PATTERN = /\A^(?<bank_id>\d{1,2})[- ]?(?<bank_branch>\d{1,4})[- ]?(?<account_base_number>\d{1,8})[- ]?(?<account_suffix>\d{1,4})\z/
 
   RADIX = 10
 
   CHECKSUM_DIGITS = 18
 
-  ACCOUNT_BASE_NUMBER_CUTOFF = 990000
+  ACCOUNT_BASE_NUMBER_CUTOFF = 990_000
 
-  BANKS = { 1 => { ranges: [1..999, 1100..1199, 1800..1899] },
-            2 => { ranges: [1..999, 1200..1299] },
-            3 => { ranges: [1..999, 1300..1399, 1500..1599, 1700..1799, 1900..1999] },
-            6 => { ranges: [1..999, 1400..1499] },
-            8 => { ranges: [6500..6599], algo: :d },
-            9 => { ranges: [], algo: :e }, # range was 0000
-            11 => { ranges: [5000..6499, 6600..8999] },
-            12 => { ranges: [3000..3299, 3400..3499, 3600..3699] },
-            13 => { ranges: [4900..4999] },
-            14 => { ranges: [4700..4799] },
-            15 => { ranges: [3900..3999] },
-            16 => { ranges: [4400..4499] },
-            17 => { ranges: [3300..3399] },
-            18 => { ranges: [3500..3599] },
-            19 => { ranges: [4600..4649] },
-            20 => { ranges: [4100..4199] },
-            21 => { ranges: [4800..4899] },
-            22 => { ranges: [4000..4049] },
-            23 => { ranges: [3700..3799] },
-            24 => { ranges: [4300..4349] },
-            25 => { ranges: [2500..2599], algo: :f },
-            26 => { ranges: [2600..2699], algo: :g },
-            27 => { ranges: [3800..3849] },
-            28 => { ranges: [2100..2149], algo: :g },
-            29 => { ranges: [2150..2299], algo: :g },
-            30 => { ranges: [2900..2949] },
-            31 => { ranges: [2800..2849], algo: :x },
-            33 => { ranges: [6700..6799], algo: :f },
-            35 => { ranges: [2400..2499] },
-            38 => { ranges: [9000..9499] } }.freeze
+  BANKS = {
+    1  => BankDefinition.new([1..999, 1100..1199, 1800..1899]),
+    2  => BankDefinition.new([1..999, 1200..1299]),
+    3  => BankDefinition.new([1..999, 1300..1399, 1500..1599, 1700..1799, 1900..1999]),
+    6  => BankDefinition.new([1..999, 1400..1499]),
+    8  => BankDefinition.new([6500..6599], :d),
+    9  => BankDefinition.new([0..0], :e),
+    11 => BankDefinition.new([5000..6499, 6600..8999]),
+    12 => BankDefinition.new([3000..3299, 3400..3499, 3600..3699]),
+    13 => BankDefinition.new([4900..4999]),
+    14 => BankDefinition.new([4700..4799]),
+    15 => BankDefinition.new([3900..3999]),
+    16 => BankDefinition.new([4400..4499]),
+    17 => BankDefinition.new([3300..3399]),
+    18 => BankDefinition.new([3500..3599]),
+    19 => BankDefinition.new([4600..4649]),
+    20 => BankDefinition.new([4100..4199]),
+    21 => BankDefinition.new([4800..4899]),
+    22 => BankDefinition.new([4000..4049]),
+    23 => BankDefinition.new([3700..3799]),
+    24 => BankDefinition.new([4300..4349]),
+    25 => BankDefinition.new([2500..2599], :f),
+    26 => BankDefinition.new([2600..2699], :g),
+    27 => BankDefinition.new([3800..3849]),
+    28 => BankDefinition.new([2100..2149], :g),
+    29 => BankDefinition.new([2150..2299], :g),
+    30 => BankDefinition.new([2900..2949]),
+    31 => BankDefinition.new([2800..2849], :x),
+    33 => BankDefinition.new([6700..6799], :f),
+    35 => BankDefinition.new([2400..2499]),
+    38 => BankDefinition.new([9000..9499])
+  }.freeze
 
   ALGOS = {
     a: [0, 0, 6, 3, 7, 9, 0, 0, 10, 5, 8, 4, 2, 1, 0, 0, 0, 0, 11],
@@ -49,7 +53,7 @@ class NzBankAccountValidator
     e: [0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 5, 4, 3, 2, 0, 0, 0, 1, 11],
     f: [0, 0, 0, 0, 0, 0, 0, 1,  7, 3, 1, 7, 3, 1, 0, 0, 0, 0, 10],
     g: [0, 0, 0, 0, 0, 0, 0, 1,  3, 7, 1, 3, 7, 1, 0, 3, 7, 1, 10],
-    x: [0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  1],
+    x: [0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
   }.freeze
 
   def self.valid?(string)
@@ -84,17 +88,16 @@ class NzBankAccountValidator
     BANKS.key?(bank_id)
   end
 
+  def bank_definition
+    BANKS[bank_id]
+  end
+
   def valid_bank_branch?
-    return false unless valid_bank_id?
+    return false unless bank_definition
     return false unless bank_branch
 
-    if BANKS[bank_id][:ranges].empty?
-      # Bank 9. Anything is valid? # TODO: confirm
-      true
-    else
-      BANKS[bank_id][:ranges].any? do |range|
-        range.include?(bank_branch)
-      end
+    bank_definition.ranges.any? do |range|
+      range.include?(bank_branch)
     end
   end
 
@@ -105,7 +108,7 @@ class NzBankAccountValidator
   def algo_code
     # If the account base number is below 00990000 then apply algorithm A
     # Otherwise apply algorithm B.
-    BANKS[bank_id][:algo] || (account_base_number < ACCOUNT_BASE_NUMBER_CUTOFF ? :a : :b)
+    bank_definition.algo || (account_base_number < ACCOUNT_BASE_NUMBER_CUTOFF ? :a : :b)
   end
 
   def algo
@@ -113,7 +116,7 @@ class NzBankAccountValidator
   end
 
   def number_for_checksum
-    format("%02d%04d%08d%04d", bank_id, bank_branch, account_base_number, account_suffix)
+    format('%02d%04d%08d%04d', bank_id, bank_branch, account_base_number, account_suffix)
   end
 
   def checksum
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
-require "codeclimate-test-reporter"
+require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
 
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-require "nz_bank_account_validator"
+require 'nz_bank_account_validator'
