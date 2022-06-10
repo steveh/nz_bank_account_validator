250a26f7025c9ae7db9686ef54e6ed3fcb4c2605
Ben Anderson <benanderson@acidic.co.nz>
2022-06-10 14:57:43 +1200

    feat: Support using either the more common NZBN format or IBAN format
    The IRD document referenced in the README is an implementation of the
    NZ styled international banking format - which isn't commonly in use in
    New Zealand is pretty useless is a local format where we can make more
    assumptions to get a wider range of valid account numbers.

--- spec/nz_bank_account_validator_spec.rb
+++ spec/nz_bank_account_validator_spec.rb
@@ -3,11 +3,11 @@
 require 'spec_helper'
 
 RSpec.describe NzBankAccountValidator do
-  def validator(string)
-    NzBankAccountValidator.new(string)
+  def validator(string, iban: false)
+    NzBankAccountValidator.new(string, iban: iban)
   end
 
-  let(:ex1) { validator('01-902-0068389-00') }
+  let(:ex1) { validator('01-902-0068389-00', iban: true) }
   let(:ex2) { validator('08-6523-1954512-001') }
   let(:ex3) { validator('26-2600-0320871-032') }
   let(:ex4) { validator('389004055424701') }
@@ -30,12 +30,12 @@ RSpec.describe NzBankAccountValidator do
       # VALIDS
       expect(validator('03-0123-0034141-03')).to be_valid_bank_branch
       expect(validator('26-2600-0034141-03')).to be_valid_bank_branch
-      expect(validator('26-2699-0034141-0003')).to be_valid_bank_branch
-      expect(validator('11-6666-0034141-0003')).to be_valid_bank_branch
+      expect(validator('26-2699-0034141-0003', iban: true)).to be_valid_bank_branch
+      expect(validator('11-6666-0034141-0003', iban: true)).to be_valid_bank_branch
 
       # INVALIDS
-      expect(validator('11-1111-0034141-0003')).to_not be_valid_bank_branch
-      expect(validator('01-2012-0034141-0003')).to_not be_valid_bank_branch
+      expect(validator('11-1111-0034141-0003', iban: true)).to_not be_valid_bank_branch
+      expect(validator('01-2012-0034141-0003', iban: true)).to_not be_valid_bank_branch
     end
   end
 
@@ -55,7 +55,7 @@ RSpec.describe NzBankAccountValidator do
       expect(ex1.algo_code).to eq(:a)
       expect(ex2.algo_code).to eq(:d)
       expect(ex3.algo_code).to eq(:g)
-      expect(ex4.algo_code).to eq(:b)
+      expect(ex4.algo_code).to eq(:a)
     end
   end
 
